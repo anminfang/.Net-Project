62efa0f54dd8cea997bed75303b01d62fe8df8b6
anminfang <36774047+anminfang@users.noreply.github.com>
2018-07-17 22:45:42 -0600

    Create ClsFillingStation_EventArgs_FuelTankRefilling.vb

---
+++ ClsFillingStation_EventArgs_FuelTankRefilling.vb
@@ -0,0 +1,158 @@
+'Copyright (c) 2009-2017 Dan Turk
+
+#Region "Class / File Comment Header block"
+'Program:                proj04-FillingStation-Fang-Anmin
+'File:                   ClsFillingStation_EventArgs_FuelTankRefilling.vb
+'Author:                 Anmin Fang
+'Description:            This is a class to inherit eventargs.
+'Date:                   2017 Dec 1
+'                          - Added attributes, get/set methods, constructors, toString methods.
+'Tier:                   Business Logic
+'Exceptions:             None generated.
+'Exception-Handling:     None.
+'Events:                 None generated.
+'Event-Handling:         None.   
+#End Region 'Class / File Comment Header block
+
+#Region "Option / Imports"
+Option Explicit On      'Must declare variables before using them
+Option Strict On        'Must perform explicit data type conversions
+#End Region 'Option / Imports
+
+Public Class FillingStation_EventArgs_FuelTankRefilled
+    Inherits EventArgs
+
+#Region "Attributes"
+    '******************************************************************
+    'Attributes + Module-level Constants+Variables
+    '******************************************************************
+
+    '********** Module-level constants
+
+    '********** Module-level variables
+
+    Private mTheProduct As Product
+
+#End Region 'Attributes
+
+#Region "Constructors"
+    '******************************************************************
+    'Constructors
+    '******************************************************************
+
+    'These are all public.
+
+    '********** Default constructor
+    '             - no parameters
+
+    '********** Special constructor(s)
+    '             - typically constructors have parameters 
+    '               that are used to initialize attributes
+
+    Public Sub New(
+           ByVal pTheProduct As Product
+           )
+        MyBase.New
+
+        _theProduct = pTheProduct
+
+    End Sub 'New(pTheProduct)
+
+    '********** Copy constructor(s)
+    '             - one parameter, an object of the same class
+
+#End Region 'Constructors
+
+#Region "Get/Set Methods"
+    '******************************************************************
+    'Get/Set Methods
+    '******************************************************************
+
+    '********** Public Get/Set Methods
+    '             - call private get/set methods to implement
+
+    Public ReadOnly Property theProduct As Product
+        Get
+            Return _theProduct
+        End Get
+    End Property
+
+    '********** Private Get/Set Methods
+    '             - access attributes, begin name with underscore (_)
+
+    Private Property _theProduct As Product
+        Get
+            Return mTheProduct
+        End Get
+        Set(ByVal pValue As Product)
+            mTheProduct = pValue
+        End Set
+    End Property
+
+#End Region 'Get/Set Methods
+
+#Region "Behavioral Methods"
+    '******************************************************************
+    'Behavioral Methods
+    '******************************************************************
+
+    '********** Public Shared Behavioral Methods
+
+    '********** Private Shared Behavioral Methods
+
+    '********** Public Non-Shared Behavioral Methods
+
+    Public Overrides Function ToString() As String
+
+        Return _toString()
+
+    End Function 'ToString()
+
+    '********** Private Non-Shared Behavioral Methods
+
+    Private Function _toString() As String
+
+        Dim tempStr As String
+
+        tempStr = "( PRODUCT EVENT_ARGS PRODUCT CREATED: " _
+            & "Product=" & _theProduct.ToString _
+            & " )"
+
+        Return tempStr
+
+    End Function '_toString()
+
+#End Region 'Behavioral Methods
+
+#Region "Event Procedures"
+    '******************************************************************
+    'Event Procedures
+    '******************************************************************
+
+    'No Event Procedures are currently defined.
+    'These are all private.
+
+    '********** User-Interface Event Procedures
+    '             - Initiated explicitly by user
+
+    '********** User-Interface Event Procedures
+    '             - Initiated automatically by system
+
+    '********** Business Logic Event Procedures
+    '             - Initiated as a result of business logic
+    '               method(s) running
+
+#End Region 'Event Procedures
+
+#Region "Events"
+    '******************************************************************
+    'Events
+    '******************************************************************
+
+    'No Events are currently defined.
+    'These are all public.
+
+#End Region 'Events
+
+End Class 'FillingStation_EventArgs_FuelTankRefilled
+
